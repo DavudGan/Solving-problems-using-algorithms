acc5f4d90a3aeac441632114c4a8ecdefd81b44e
Davud <davud.ganbaroff@yandex.ru>
2024-07-30 23:20:25 +0500

    find-pair-by-sum

---
+++ find-pair-by-sum.js
@@ -0,0 +1,22 @@
+/*
+ Нахождение пары чисел, дающую нужную сумму (через словарь).
+*/
+
+const array = [5, 2, 3, 4, 9];
+
+const summ = (array, summ) => {
+  const s = {};
+
+  for (let i = 0; i < array.length; i++) {
+    const possiblePair = summ - array[i];
+    if (s[array[i]] === undefined) {
+      s[possiblePair] = i;
+    } else {
+      return console.log(s[array[i]] + " and " + i);
+    }
+  }
+
+  return null;
+};
+
+summ(array, 8);
